fa75dd1026fa8db38495720caf288f6d6480aa8f
Jimmy Malhan <jimmymalhan999@gmail.com>
2023-07-02 10:14:42 -0700

    adding data

--- check_and_delete.sh
+++ check_and_delete.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Check if port 8000 is in use
-if lsof -Pi :8000 -sTCP:LISTEN -t >/dev/null; then
+if lsof -Pi :8000 -sTCP:LISTEN >/dev/null; then
   # Get the PID of the process using port 8000
   pid=$(lsof -Pi :8000 -sTCP:LISTEN -t)
 
